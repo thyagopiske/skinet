85a864336f083db85ae1a3a24e1682a9d41d11c6
thyago <thyagopiske@gmail.com>
2022-09-11 02:49:21 -0300

    aplicar migration, criar banco e seed de dados ao iniciar a aplicação

--- Api/Program.cs
+++ Api/Program.cs
@@ -23,6 +23,25 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
+//Apply migrations, create database and seed data at startup
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    var logger = loggerFactory.CreateLogger<Program>();
+    try
+    {
+        var storeContext = services.GetRequiredService<StoreContext>();
+        await storeContext.Database.MigrateAsync();
+        logger.LogInformation("Migration verification completed!");
+        await StoreContextSeed.SeedAsync(storeContext, loggerFactory);
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, "An error ocorrued during migration verification or execution");
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,64 @@
+﻿using Core.Entities;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
+        {
+            var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+
+            try
+            {
+                if (!context.ProductBrands.Any())
+                {
+                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+
+                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+
+                    await context.ProductBrands.AddRangeAsync(brands);
+
+                    await context.SaveChangesAsync();
+
+                    logger.LogInformation("ProductBrands seed done!");
+                }
+
+                if (!context.ProductTypes.Any())
+                {
+                    var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+
+                    var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+
+                    await context.ProductTypes.AddRangeAsync(types);
+
+                    await context.SaveChangesAsync();
+
+                    logger.LogInformation("ProductTypes seed done!");
+                }
+
+                if (!context.Products.Any())
+                {
+                    var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+
+                    var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+                    await context.Products.AddRangeAsync(products);
+
+                    await context.SaveChangesAsync();
+
+                    logger.LogInformation("Products seed done!");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+            }
+        }
+    }
+}
